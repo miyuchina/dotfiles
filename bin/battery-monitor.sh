68e9931f704705ad64952bf7a70a314c4ab2a632
Mi Yu <miyu.chs@gmail.com>
2019-04-29 00:07:02 -0400

    revert i3bar

--- bin/battery-monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-battery_level=$(acpi -b | head -1 | cut -d ' ' -f 4 | grep -o '[0-9]*')
-
-if [ $battery_level -le 10 ]; then
-    dunstify -u critical "Low Battery" "Your computer will go to sleep soon."
-fi
